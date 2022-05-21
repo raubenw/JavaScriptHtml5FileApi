2fb3da6df953d7deac824b8a1c4a90a878efb370
raubenw <91202958+raubenw@users.noreply.github.com>
2022-05-21 03:28:03 +0200

    Read only specific file types

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ let file = document.querySelector('#file');
 
 function readFile(e) {
     let file = e.target.files[0];
-    console.log(file);
+   
+    if(file.type === "text/plain") 
+    {
+        let reader = new FileReader();
+        reader.onload = function (e) 
+        {
+            let content = e.target.result;
+            console.log(content);
+            //console.log(e);
+        }
+        reader.readAsText(file);
+    }
 }
 
 file.addEventListener('change', readFile, false);
