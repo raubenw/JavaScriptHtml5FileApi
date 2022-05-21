65f829ca7406c5cc8bea870496cde6e72514f33b
raubenw <91202958+raubenw@users.noreply.github.com>
2022-05-21 21:06:13 +0200

    Select and read multiple files

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 let file = document.querySelector('#file');
 
-function readFile(e) {
-    let file = e.target.files[0];
-   
-    if(file.type === "text/plain") 
+function read(file) 
+{    
+    let reader = new FileReader();
+
+    reader.onload = function (e) 
     {
-        let reader = new FileReader();
-        reader.onload = function (e) 
-        {
-            let content = e.target.result;
-            console.log(content);
-            //console.log(e);
-        }
-        reader.readAsText(file);
+        let content = e.target.result;
+        console.log(content);
+        //console.log(e);
+    }
+    reader.readAsText(file);
+}
+
+
+function readFile(e) 
+{
+    for(let x = 0; x < e.target.files.length; x++) {
+        read(e.target.files[x]);
     }
 }
 
